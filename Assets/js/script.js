d01939afe4e5f72aa238d6e7b8e928a22e0628f3
Abigail Adams <abigail-adams@gmail.com>
2021-04-06 21:13:30 -0700

    styling

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,61 +2,23 @@ var cityName = document.getElementById("#search-value");
 var uvresults;
 var formEl = document.querySelector("#search-city");
 var DashboardEl = document.querySelector("#jumbotron");
-
-// function makeRow(searchValue) {
-//   var liEl = document.createElement("li")
-//   liEl.classList.add("list-group-item", "list-group-item-action");
-//   var text = searchValue;
-//   liEl.textContent = text;
-//   var historyEl = document.querySelector('.history');
-//   console.log(event.target)
-//   historyEl.onclick = function(){
-//     console.log(event.target.tagName)
-//     if (event.target.tagName == "LI"){
-//     searchWeather(event.target.textContent)
-//     }
-//   }
-//   historyEl.appendChild(liEl);
-// };
-
-// var geocoder;
-// geocoder = new google.maps.Geocoder();
-// var latlng = new google.maps.LatLng(latitude, longitude);
-
-// geocoder.geocode(
-// {'latLng': latlng},
-// function(results, status) {
-//     if (status == google.maps.GeocoderStatus.OK) {
-//             if (results[0]) {
-//                 var add= results[0].formatted_address ;
-//                 var  value=add.split(",");
-
-//                 count=value.length;
-//                 country=value[count-1];
-//                 state=value[count-2];
-//                 city=value[count-3];
-//                 alert("city name is: " + city);
-//             }
-//             else  {
-//                 alert("address not found");
-//             }
-//     }
-//      else {
-//         alert("Geocoder failed due to: " + status);
-//     }
-// }
-
-// function GetFormattedDate() {
-//   var todayTime = new Date();
-//   var month = format(todayTime.getMonth() + 1);
-//   var day = format(todayTime.getDate());
-//   var year = format(todayTime.getFullYear());
-//   return month + "/" + day + "/" + year;
-// }
+var cityList = JSON.parse(localStorage.getItem("cityList")) || [];
 
 function displayDashboard(event) {
   event.preventDefault();
   var cityName = document.getElementById("search-value").value;
+
+  cityList.push(cityName);
+
+  localStorage.setItem("cityList", JSON.stringify(cityList));
+  for (let i = 0; i < cityList.length; i++) {
+    JSON.parse(window.localStorage.getItem("cityList"));
+    // var cityNamesEl = document.querySelector(".cityNames");
+    // cityNamesEl.textContent = cityList[i].cityList;
+    // cityNamesEl.append(cityList);
+    // document.getElementById("demo").innerHTML = highScore["0:i"];
+  }
+  // for loop then generate
   fetch(
     "https://api.openweathermap.org/data/2.5/weather?q=" +
       cityName +
@@ -76,16 +38,17 @@ function displayDashboard(event) {
         })
         .then(function (response) {
           console.log(response);
-          var titleEl = document.createElement("h3");
+          var titleEl = document.createElement("h2");
           document.querySelector(".jumbotron").innerHTML = `
-
+          <h1> Current Weather:
+          <h5>
             ${results.name} ~ ${moment(results.dt, "X").format("LL")} 
             <img src="http://openweathermap.org/img/w/${
               response.current.weather[0].icon
-            }.png" alt="">
-          <p id="temp">Temperature: ${response.current.temp}&deg; F </p>
-          <p id="humidity">Humidity: ${response.current.humidity} % </p>
-          <p id="windSpeed">Wind Speed: ${response.current.wind_speed} MPH </p>
+            }.png" alt=""></h5></h1>
+          <p id="temp"> Temperature: ${response.current.temp}&deg; F </p>
+          <p id="humidity"> Humidity: ${response.current.humidity} % </p>
+          <p id="windSpeed"> Wind Speed: ${response.current.wind_speed} MPH </p>
           <p id="UV"> UV Index: ${response.current.uvi} </p>
           `;
 
@@ -131,36 +94,6 @@ function fiveDayForecast(cityName) {
               `;
         }
       }
-      // document.getElementById("monday").innerHTML = `
-      //       <h5 class="card-title">3/8/2021</h5>
-      //       <img src="http://openweathermap.org/img/w/${results.list[1].icon}.png" alt="">
-      //       <p class="card-text">Temp: ${results.list[0].main.temp}&deg; F</p>
-      //       <p class="card-text"><small class="text-muted">Humidity 20 %</small></p>
-      //   `;
-      // document.getElementById("tuesday").innerHTML = `
-      //   <h5 class="card-title">3/8/2021</h5>
-      //   <img src="http://openweathermap.org/img/w/${results.list[1].icon}.png" alt="">
-      //   <p class="card-text">Temp: 50 &deg; F</p>
-      //   <p class="card-text"><small class="text-muted">Humidity 20 %</small></p>
-      //   `;
-      // document.getElementById("wednesday").innerHTML = `
-      //   <h5 class="card-title">3/8/2021</h5>
-      //   <img src="http://openweathermap.org/img/w/${results.list[1].icon}.png" alt="">
-      //   <p class="card-text">Temp: 50 &deg; F</p>
-      //   <p class="card-text"><small class="text-muted">Humidity 20 %</small></p>
-      //     `;
-      // document.getElementById("thursday").innerHTML = `
-      // <h5 class="card-title">3/8/2021</h5>
-      // <img src="http://openweathermap.org/img/w/${results.list[1].icon}.png" alt="">
-      // <p class="card-text">Temp: 50 &deg; F</p>
-      // <p class="card-text"><small class="text-muted">Humidity 20 %</small></p>
-      // `;
-      // document.getElementById("friday").innerHTML = `
-      // <h5 class="card-title">3/8/2021</h5>
-      // <img src="http://openweathermap.org/img/w/${results.list[1].icon}.png" alt="">
-      // <p class="card-text">Temp: 50 &deg; F</p>
-      // <p class="card-text"><small class="text-muted">Humidity 20 %</small></p>
-      // `;
     });
 }
 formEl.addEventListener("submit", displayDashboard);
